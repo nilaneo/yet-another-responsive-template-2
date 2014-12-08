418c621951068800599926f431de92f0de609e6e
unknown <n.shlikhtenko@nshlikhtenko.kiev.astoundcommerce.com>
2014-12-08 13:50:07 +0200

    Made the site fully responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-$(document).ready( function() {
-  $(".j-btn-nav").on ("click", function() {
-    $(".nav").toggleClass("is-opened");
-  });
+$(document).ready(function() {
+    $('select').fancySelect();
+    $('.jcarousel')
+        .on('jcarousel:create jcarousel:reload', function() {
+            var element = $(this),
+                width = element.innerWidth();
+
+            if (width > 700) {
+                width = (width - 60) / 3;
+            } else if (width > 500) {
+                width = (width - 40) / 2;
+            } else if (width < 499) {
+                width = width;
+            }
+
+            element.jcarousel('items').css('width', width + 'px');
+        });
+
+
+    $('.jcarousel-control-prev')
+    .jcarouselControl({
+        target: '-=1',
+    });
+
+    $('.jcarousel-control-next')
+    .jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel-control').on('jcarouselcontrol:inactive', function() {
+        $(this).addClass("inactive");
+    });
+
+    $('.jcarousel-control').on('jcarouselcontrol:active', function() {
+        $(this).removeClass("inactive");
+    });
+
+    $('.jcarousel').jcarousel();
 });
